118f1e2957747de186dc0cbeecb3bfadc19636f2
jborseth <jborseth@users.noreply.github.com>
2015-05-15 14:58:18 -0400

    Add methods/market_data/currencies/01_get_currencies.js

--- methods/market_data/currencies/01_get_currencies.js
+++ methods/market_data/currencies/01_get_currencies.js
@@ -4,3 +4,15 @@
  * Docs:
  *   https://docs.exchange.coinbase.com/#get-currencies
  */
+
+var publicClient = require('../../../client').publicClient;
+
+
+publicClient.getCurrencies(function(err, response) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(response.body);
+});
