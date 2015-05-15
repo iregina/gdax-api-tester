3b5486548f0cc9cdf22cac3a45c5df1c3a119023
jborseth <jborseth@users.noreply.github.com>
2015-05-15 14:56:52 -0400

    Add methods/market_data/time/01_time.js

--- methods/market_data/time/01_time.js
+++ methods/market_data/time/01_time.js
@@ -4,3 +4,15 @@
  * Docs:
  *   https://docs.exchange.coinbase.com/#time
  */
+
+var publicClient = require('../../../client').publicClient;
+
+
+publicClient.getTime(function(err, response) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(response.body);
+});
