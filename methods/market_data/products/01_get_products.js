41926982dd1aaed9390c2a77ad7787d9c9164cb1
jborseth <jborseth@users.noreply.github.com>
2015-05-15 15:00:04 -0400

    Add methods/market_data/products/01_get_products.js

--- methods/market_data/products/01_get_products.js
+++ methods/market_data/products/01_get_products.js
@@ -4,3 +4,15 @@
  * Docs:
  *   https://docs.exchange.coinbase.com/#get-products
  */
+
+var publicClient = require('../../../client').publicClient;
+
+
+publicClient.getProducts(function(err, response) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(response.body);
+});
