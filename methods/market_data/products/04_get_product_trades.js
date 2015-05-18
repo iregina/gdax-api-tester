f820e3cb0e2bc120f2811a6b539a838336538483
jborseth <jborseth@users.noreply.github.com>
2015-05-18 15:28:04 -0400

    Use only one products/04 example

--- methods/market_data/products/04_get_product_trades.js
+++ methods/market_data/products/04_get_product_trades.js
@@ -8,18 +8,6 @@
 var publicClient = require('../../../client').publicClient;
 
 
-// coinbase-exchange's Read Me says this should work. Currently does not. Issue created.
-// publicClient.getProductTrades(function(err, response) {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-
-//   console.log(response.body);
-// });
-
-
-// To make paginated requests, include page parameters
 publicClient.getProductTrades({after: 1000}, function(err, response, data) {
   if (err) {
     console.log(err);
