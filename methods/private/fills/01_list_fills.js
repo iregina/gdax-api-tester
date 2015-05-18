6036aac530ffea31d1faf59aec0f834058364e96
jborseth <jborseth@users.noreply.github.com>
2015-05-18 14:58:18 -0400

    Add fills/01_list_fills.js

--- methods/private/fills/01_list_fills.js
+++ methods/private/fills/01_list_fills.js
@@ -4,3 +4,15 @@
  * Docs:
  *   https://docs.exchange.coinbase.com/#list-fills
  */
+
+var authedClient  = require('../../../client.js').authedClient;
+
+
+authedClient.getFills(function(err, response, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(data);
+});
