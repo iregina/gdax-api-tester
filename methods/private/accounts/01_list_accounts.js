46000d82aca755b085321b0644b9945bcd58cdfb
jborseth <jborseth@users.noreply.github.com>
2015-05-18 14:29:57 -0400

    Add private/accounts/01_list_accounts.js

--- methods/private/accounts/01_list_accounts.js
+++ methods/private/accounts/01_list_accounts.js
@@ -4,3 +4,15 @@
  * Docs:
  *   https://docs.exchange.coinbase.com/#list-accounts
  */
+
+var authedClient = require('../../../client').authedClient;
+
+
+authedClient.getAccounts(function(err, response, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(data);
+});
