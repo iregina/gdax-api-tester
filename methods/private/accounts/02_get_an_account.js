f0eb6bcd0efe4d46cbbb5f0a06104a20cbdebc00
jborseth <jborseth@users.noreply.github.com>
2015-05-18 14:53:05 -0400

    Add accounts/02_get_an_account.js

---
+++ methods/private/accounts/02_get_an_account.js
@@ -0,0 +1,36 @@
+/**
+ * Get an account
+ *
+ * Docs:
+ *   https://docs.exchange.coinbase.com/#get-an-account
+ */
+
+var async = require('async');
+var authedClient = require('../../../client').authedClient;
+
+
+async.waterfall([
+  function(callback) {
+
+    authedClient.getAccounts(function(err, response, accounts) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      callback(null, accounts[0].id);
+    });
+
+  }, function(accountId) {
+
+    authedClient.getAccount(accountId, function(err, response, data) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      console.log(data);
+    });
+
+  }
+]);
