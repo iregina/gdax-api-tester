7d7737fe75185602d03ee04b4874c573a33ba474
jborseth <jborseth@users.noreply.github.com>
2015-05-18 14:39:45 -0400

    Add private/orders/04_get_an_order.js

---
+++ methods/private/orders/04_get_an_order.js
@@ -0,0 +1,42 @@
+/**
+ * Get an order
+ *
+ * Docs:
+ *   https://docs.exchange.coinbase.com/#get-an-order
+ */
+
+var authedClient = require('../../../client').authedClient;
+var async = require('async');
+
+
+async.waterfall([
+  function(callback) {
+
+    authedClient.getOrders(function(err, response, orders) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      if (orders.length === 0) {
+        console.log('You have no orders');
+        return;
+      }
+
+      callback(null, orders[0].id);
+    });
+
+  }, function(orderId) {
+
+    // Alternatively, you can manually specify an order ID here
+    // var orderId = '';
+
+    authedClient.getOrder(orderId, function(err, response, order) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(order);
+    });
+  }
+]);
