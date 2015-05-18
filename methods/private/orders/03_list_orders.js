8d1215794258dc65a6c5546d0434211f779125c6
jborseth <jborseth@users.noreply.github.com>
2015-05-18 14:32:43 -0400

    Add private/orders/03_list_orders.js

---
+++ methods/private/orders/03_list_orders.js
@@ -0,0 +1,28 @@
+/**
+ * List orders
+ *
+ * Docs:
+ *   https://docs.exchange.coinbase.com/#list-orders
+ */
+
+var authedClient = require('../../../client').authedClient;
+
+
+authedClient.getOrders(function(err, response, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(data);
+});
+
+// For pagination, you can include extra page arguments
+// authedClient.getOrders({after: 3000}, function(err, response, data) {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+
+//   console.log(data);
+// });
